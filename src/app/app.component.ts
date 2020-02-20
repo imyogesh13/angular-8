3e826283bcfeb4c8e42b27f6de9c7dcc318598d2
imyogesh13 <patil.yps@gmail.com>
2020-02-20 11:59:13 +0530

    3 files modified, 3 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  parentName = 'Angular from parent comp';
+    parentMsg = 'Angular from parent component';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { TestComponent } from './test.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, TestComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule { 
+  parentMsg : string = "Message from parent component";
+
+}
