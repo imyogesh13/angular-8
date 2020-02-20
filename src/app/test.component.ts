895feaaace11f0f90a268b7b2ff13517fbca2167
imyogesh13 <patil.yps@gmail.com>
2020-02-20 12:23:51 +0530

    4 files modified

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -7,5 +7,16 @@ import {Component} from '@angular/core';
 })
 
 export class TestComponent {
+public name ="txtname";
+myid="testid";
+public successClass = "text-success";
+textSpecial ="text-special";
+hasError : boolean = true;
+isSpecial =true;
 
+message ={
+  "text-success" : !this.hasError,
+  "text-danger" : this.hasError,
+  "text-special" :this.isSpecial
+}
 }
