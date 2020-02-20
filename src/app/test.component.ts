1856c68392302ffb4ed163f73a7406afdc6d5d6d
imyogesh13 <patil.yps@gmail.com>
2020-02-20 14:00:02 +0530

    2 files modified

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -8,15 +8,24 @@ import {Component} from '@angular/core';
 
 export class TestComponent {
 public name ="txtname";
-myid="testid";
+public myid="testid";
 public successClass = "text-success";
-textSpecial ="text-special";
-hasError : boolean = true;
-isSpecial =true;
+public textSpecial ="text-special";
+public hasError : boolean = true;
+public isSpecial =true;
+public bindMsg :string ="";
+  message ={
+    "text-success" : !this.hasError,
+    "text-danger" : this.hasError,
+    "text-special" :this.isSpecial
+  }
 
-message ={
-  "text-success" : !this.hasError,
-  "text-danger" : this.hasError,
-  "text-special" :this.isSpecial
-}
+  ShowMsg(event)
+  {
+    console.log(event);
+  }
+
+  logMessage(value){
+    console.log(value);
+  }
 }
