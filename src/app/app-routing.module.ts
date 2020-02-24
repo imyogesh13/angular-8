9f6cab4621cd78f757df01f303ea831643eeba38
imyogesh13 <patil.yps@gmail.com>
2020-02-24 15:26:43 +0530

    1 file modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router'
 import {EmpListComponent} from './emp-list.component';
 import {EmpDetailsComponent} from './emp-details.component';
+import {PageNotFoundComponent} from './pagenotfound.component';
 const routes:Routes =[
   {path:"employeelist", component: EmpListComponent},
-  {path:"employeedetails", component: EmpDetailsComponent}
+  {path:"employeedetails", component: EmpDetailsComponent},
+  {path:"**", component:PageNotFoundComponent}
 ];
 @NgModule({
   imports:[
@@ -14,4 +16,4 @@ const routes:Routes =[
 })
 
 export class AppRoutingModule{}
-export const routingComponent =[EmpListComponent, EmpDetailsComponent]; 
+export const routingComponent =[EmpListComponent, EmpDetailsComponent,PageNotFoundComponent]; 
---
+++ src/app/pagenotfound.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input,Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector :"app-pagenotfound",
+  templateUrl: "./pagenotfound.component.html"
+})
+
+export class PageNotFoundComponent{
+   constructor(){
+   }
+   ngOnInit(){
+   }
+}
