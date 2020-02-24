deca8ca859c86efc7e0d7c5030df491c914655f0
imyogesh13 <patil.yps@gmail.com>
2020-02-24 17:04:57 +0530

    2 files modified

--- src/app/departmentdetails.component.ts
+++ src/app/departmentdetails.component.ts
@@ -1,16 +1,33 @@
 import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector :"app-departmentdetails",
   templateUrl: "./departmentdetails.component.html",
+  
 })
 
 export class DepartmentDetailsComponent{
-  public departmetId = '';
-   constructor(private route:ActivatedRoute){
+  public departmetId ;
+   constructor(private route:ActivatedRoute, private router:Router){
    }
    ngOnInit(){
-     this.departmetId = this.route.snapshot.paramMap.get("id");
+     let id = parseInt( this.route.snapshot.paramMap.get("id"));
+     this.departmetId = id;
+   }
+   goPrevious(){
+      let id = this.departmetId - 1;
+      console.log(id);
+
+      this.router.navigate(['/departments',id]);
+       //this.router.navigate(['/departments',dept.deptId]);
+   }
+
+   goNext(){
+      let id = this.departmetId +1;
+      this.router.navigate(['/departments',id]);
+   }
+   goBackToDeptList(){
+     this.router.navigate(['/departments']);
    }
 }
