7eb6fbbcdd809cf1285145b3e04f1ed5d1a68eb4
imyogesh13 <patil.yps@gmail.com>
2020-02-25 14:21:59 +0530

    1 file modified

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.get('/', function(req, res){
 
 app.post('/enroll', function(req,res){
    console.log(req.body);
+   res.status(200).send({"message": "Data recieved!!"});
 })
 app.listen(PORT,function(){
   console.log("Server running on localhost:"+PORT);
