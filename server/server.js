f775ec6061d29f4f3aaea4001c0e79997564f8aa
imyogesh13 <patil.yps@gmail.com>
2020-02-25 14:20:56 +0530

    1 file modified

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ app.get('/', function(req, res){
  res.send("Hello from express");
 });
 
+app.post('/enroll', function(req,res){
+   console.log(req.body);
+})
 app.listen(PORT,function(){
   console.log("Server running on localhost:"+PORT);
 })
