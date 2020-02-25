d588eefd78d56bfd91751f568e90e7bc9cc1573a
imyogesh13 <patil.yps@gmail.com>
2020-02-25 15:18:54 +0530

    2 files modified

--- src/app/ReactiveForm/registration.component.ts
+++ src/app/ReactiveForm/registration.component.ts
@@ -10,7 +10,12 @@ export class RegistrationComponent{
   registrationForm = new FormGroup({
     userName : new FormControl(''),
     password : new FormControl(''),
-    confirmPassword : new FormControl('')
+    confirmPassword : new FormControl(''),
+    address : new FormGroup({
+      city : new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+    })
   });
    constructor(){
    }
