67139a88859965237cd39567d914007acdf784de
imyogesh13 <patil.yps@gmail.com>
2020-02-25 15:42:50 +0530

    1 file modified

--- src/app/ReactiveForm/registration.component.ts
+++ src/app/ReactiveForm/registration.component.ts
@@ -1,33 +1,48 @@
 import { Component } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
 @Component({
   selector: "app-registration",
   templateUrl: "./registration.component.html"
 })
 export class RegistrationComponent {
-  registrationForm = new FormGroup({
-    userName: new FormControl(""),
-    password: new FormControl(""),
-    confirmPassword: new FormControl(""),
-    address: new FormGroup({
-      city: new FormControl(""),
-      state: new FormControl(""),
-      postalCode: new FormControl("")
+  
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {}
+  registrationForm = this.fb.group({
+    userName : [''],
+    password : [''],
+    confirmPassword : [''],
+    address : this.fb.group({
+      city : [''],
+      state : [''],
+      postalCode : ['']
     })
   });
-  constructor() {}
-  ngOnInit() {}
-
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl(""),
+  //   password: new FormControl(""),
+  //   confirmPassword: new FormControl(""),
+  //   address: new FormGroup({
+  //     city: new FormControl(""),
+  //     state: new FormControl(""),
+  //     postalCode: new FormControl("")
+  //   })
+  // });
   loadAPIData() {
-    this.registrationForm.setValue({
+    // this.registrationForm.setValue({
+    //   userName: 'Yogesh',
+    //   password: 'test',
+    //   confirmPassword: 'test',
+    //   address: {
+    //     city: 'Pune',
+    //     state: 'MH',
+    //     postalCode: 541822
+    //   }
+    // });
+    this.registrationForm.patchValue({
       userName: 'Yogesh',
       password: 'test',
-      confirmPassword: 'test',
-      address: {
-        city: 'Pune',
-        state: 'MH',
-        postalCode: 541822
-      }
+      confirmPassword: 'test'
     });
   }
 }
