8b768d5e54fa325b6d35c0da4ad179fd822249c4
imyogesh13 <patil.yps@gmail.com>
2020-02-25 11:44:27 +0530

    1 file modified, 2 added

---
+++ src/app/tdpdemo.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector :"app-tdpdemo",
+  templateUrl: "./tdpdemo.component.html"
+})
+
+export class TdpDemoComponent{
+   constructor(){
+   }
+   ngOnInit(){
+   }
+}
